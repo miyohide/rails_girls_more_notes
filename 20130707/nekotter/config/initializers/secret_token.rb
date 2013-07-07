5e53c279c4a3362514248712b3111d96be398842
Hidenori Miyoshi <miyohide@gmail.com>
2013-07-07 21:03:50 +0900

    sample source add

---
+++ 20130707/nekotter/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nekotter::Application.config.secret_token = '017639663bcb0226ddd30f1db6c7c9f61f41838f7b983d084d72c0aabfeaaf4d2bc5751480b7c85e22fa6b3e605ed5b3b90c2a31d43dbf6fe84c415de402074c'
