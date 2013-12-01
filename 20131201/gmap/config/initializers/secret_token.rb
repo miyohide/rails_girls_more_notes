1d01c485c5ea9ecef2c347b4090ab5303ddb8dcc
Hidenori Miyoshi <miyohide@gmail.com>
2013-12-01 12:00:01 +0900

    initial commit

---
+++ 20131201/gmap/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gmap::Application.config.secret_key_base = '4a95206c76467ea0e8f2408d80eac5c12c4ecbafe94c5fc39e85996dcde821be2d0d03fedb1855bf9408c58fedb5af0b898736ab257c4864483350f523ba7bdd'
---
+++ 20131201/gmap/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gmap::Application.config.session_store :cookie_store, key: '_gmap_session'
