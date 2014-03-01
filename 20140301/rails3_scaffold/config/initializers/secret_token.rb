92cf727716933ce9314ae89b3bf3637baeaaf2e5
Hidenori Miyoshi <miyohide@gmail.com>
2014-03-01 20:46:24 +0900

    Add Sample App(Rails3 ver.)

---
+++ 20140301/rails3_scaffold/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails3Scaffold::Application.config.secret_token = '4248c4b01a9c983279cf32ade61e9369753f9dbd08644343062118293b4c44420730680f537b473a9bbc71563ba26b817b78153bf0a805867e3ca87d798e3f72'
